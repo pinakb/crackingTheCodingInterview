ed6787bb7086abf847444f9568f5140c2afb7e72
pinakb <39387132+pinakb@users.noreply.github.com>
2020-05-14 13:40:04 -0700

    14 May 2020

---
+++ problem_1_6.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<set>
+#include<queue>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main()
+{
+    string input;
+    cout<<"Enter the input string: ";
+    getline(cin,input);
+
+    int counter=0;
+    string sequence="";
+
+    set<char> alphabet(input.begin(), input.end());
+    if(alphabet.size()*2 < input.length())
+    {
+        for(int i=0; i<input.length()-1;i++)
+        {
+            if(input[i]==input[i+1] && i!= input.length()-2)
+            {
+                counter++;
+            }
+            else if(input[i]==input[i+1] && i == input.length()-2)
+            {
+                counter+=2;
+                sequence+=input[i];
+                sequence += to_string(counter);
+            }
+            else if(input[i]!=input[i+1] && i == input.length()-2)
+            {
+                counter+=2;
+                sequence+=input[i];
+                sequence += to_string(counter);
+                sequence+=input[i+1];
+                counter=1;
+                sequence += to_string(counter);
+            }
+            else
+            {
+                counter++;
+                sequence+=input[i];
+                sequence += to_string(counter);
+                counter=0;
+            }
+        }
+        cout<<"Sequence: "<<sequence<<endl;
+    }
+    else
+    {
+        cout<<"sequence: "<<input<<endl;
+    }
+
+}
---
+++ problem_1_7.cpp
@@ -0,0 +1,55 @@
+//Roatate matrix by 90 degrees.
+//Solution is for Anti-clockwise rotation.
+//Input: Dimension N of matrix. (Matrix will be auto populated)
+//Output: Rotated matrix
+//Complexity = O(n square)
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the Dimension n: ";
+    cin>>n;
+
+    int mat[n][n];
+
+    for(int i=0; i<n;i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            mat[i][j]=i+j;
+        }
+    }
+    cout<<"Input Matrix: "<<endl;
+    for(int i=0; i<n;i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<mat[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    cout<<"\n Output Matrix:\n";
+    for(int i=0; i<n/2; i++)
+    {
+        for(int j=i; j< n-i-1; j++)
+        {
+            int temp = mat[i][j];
+            mat[i][j]= mat[j][n-i-1];
+            mat[j][n-i-1] = mat[n-i-1][n-j-1];
+            mat[n-i-1][n-j-1]= mat[n-j-1][i];
+            mat[n-j-1][i]=temp;
+        }
+    }
+    for(int i=0; i<n;i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<mat[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+}
