cff0705d87510bfdf82107fdbab267e1b01cb8c7
mihir <mihir@LAPTOP-HSBGP345>
2020-05-22 17:14:02 +0530

    Linked List part 2

--- Partition.java
+++ Partition.java
@@ -1,6 +1,9 @@
 package com.java.code.roop.two;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Partition {
@@ -12,55 +15,45 @@ public class Partition {
 			System.out.println("Size of Linked List: ");
 			String string1 = myObj.nextLine();
 			int size = Integer.parseInt(string1);
-			SinglyLinkedList sLL = new SinglyLinkedList();
+			List<Integer> sLL = new LinkedList<Integer>();
 			for (int i = 0; i < size; i++) {
 				System.out.println("Enter element " + i);
 				String string2 = myObj.nextLine();
-				sLL.addNode(Integer.parseInt(string2));
+				sLL.add(Integer.parseInt(string2));
 			}
-			sLL.display();
+
+			for (int temp : sLL) {
+				System.out.println(temp);
+			}
+
 			System.out.println("Enter element to pivot for partition: ");
 			String string3 = myObj.nextLine();
-			Node pivotNode = new Node(Integer.parseInt(string3));
 
 			int[] listArray = new int[size];
-			listArray[0] = (int) sLL.head.getData();
-			Node node = sLL.head.getNext();
-			int i = 1;
-			while (node.getNext() != null) {
-				listArray[i] = (int) node.getData();
-				node = node.getNext();
-				i++;
-			}
-			listArray[size-1] = (int) sLL.tail.getData();
+			for (int i = 0; i < size; i++)
+				listArray[i] = sLL.get(i);
 
 			Arrays.sort(listArray);
-			
-			int index = Arrays.binarySearch(listArray, (int)pivotNode.getData());
+
+			int index = Arrays.binarySearch(listArray, Integer.parseInt(string3));
 			int pivotPosition = (index < 0) ? -1 : index;
 
-			sLL.head = null;
-			if (pivotPosition == 0) {
-				sLL.head = new Node(pivotNode);
+			sLL.clear();
+			if (pivotPosition == 0 || pivotPosition == size) {
+				for (int i = 0; i < size; i++)
+					sLL.add(listArray[i]);
 			} else {
-				sLL.head = new Node(listArray[0]);
-				for (int j = 1; j < pivotPosition; j++)
-					sLL.tail.setNext(new Node(listArray[i]));
-				if (size == pivotPosition)
-					sLL.tail.setNext(new Node(pivotPosition));
-				else {
-					sLL.tail.setNext(new Node(listArray[pivotPosition+1]));
-					sLL.tail.setNext(new Node(listArray[pivotPosition]));
-					if (size > (pivotPosition + 2)) {
-						for (int j = pivotPosition+2; j<size;j++) {
-							sLL.tail.setNext(new Node(listArray[j]));
-						}
-					}
+				for (int j = 0; j < pivotPosition; j++)
+					sLL.add(listArray[j]);
+				sLL.add(listArray[pivotPosition + 1]);
+				sLL.add(listArray[pivotPosition]);
+				if (size >= (pivotPosition + 2)) {
+					for (int j = pivotPosition + 2; j < size; j++)
+						sLL.add(listArray[j]);
 				}
 			}
-			
-			System.out.println("Updated Linked List is ");
-			sLL.display();
+
+			System.out.println("Updated Linked List is " + sLL);
 		} finally {
 			myObj.close();
 		}
